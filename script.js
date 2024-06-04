6cfbe5f06a72bd12e9465c105ce4fe75eb7c68bc
foalant <104104058+foalant@users.noreply.github.com>
2024-06-05 01:17:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ let coins = 0;
 function collectCoins() {
     coins += Math.floor(Math.random() * 10) + 1;
     document.getElementById('coinCount').innerText = 'Coins: ' + coins;
+
+    // Анимация изменения изображения
+    let image = document.getElementById('clickerImage');
+    image.src = 'https://sun9-69.userapi.com/sun9-73/mB_jNmfFUOZUF9RpG1Hx2AIftxKBO879DCEcYw/XsskELnUErI.jpg';
+
+    // Возврат к исходному изображению после небольшой задержки
+    setTimeout(() => {
+        image.src = 'https://i.postimg.cc/wBDHJRZk/83ffb5.png';
+    }, 100);
 }
